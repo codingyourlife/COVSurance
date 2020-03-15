a34d8c4cc48053805f47aa5796739d56935c69e3
Johannes Mayer <johannes.mayer@conda.at>
2020-03-15 11:34:35 +0100

    ERC20, ERC20Detailed

--- backend/contracts/Coins/InvestorCoin.sol
+++ backend/contracts/Coins/InvestorCoin.sol
@@ -1,5 +1,11 @@
 pragma solidity ^0.5.5;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract InvestorCoin is ERC20Detailed {}
+contract InvestorCoin is ERC20, ERC20Detailed {
+    constructor(string memory name, string memory symbol, uint8 decimals)
+        public
+        ERC20Detailed(name, symbol, decimals)
+    {}
+}
