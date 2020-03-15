0e7b8f051b5842b4d31079a113396320e4ba1d35
Johannes Mayer <johannes.mayer@conda.at>
2020-03-15 10:44:42 +0100

    coins

---
+++ backend/contracts/Coins/InsureeCoin.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.5;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract InsureeCoin is ERC20Detailed {}
---
+++ backend/contracts/Coins/InvestorCoin.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.5;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract InvestorCoin is ERC20Detailed {}
