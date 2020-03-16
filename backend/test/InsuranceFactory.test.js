33c92af230599ab471abd1fca617e5315a324cfb
Johannes Mayer <johannes.mayer@conda.at>
2020-03-16 23:13:09 +0100

    InsuranceFactory investment tests into factory

--- backend/test/InsuranceFactory.test.js
+++ backend/test/InsuranceFactory.test.js
@@ -13,11 +13,16 @@ const zero_address = "0x0000000000000000000000000000000000000000";
 const InsuranceFactory = contract.fromArtifact("InsuranceFactory");
 const MoneyVaultFactory = contract.fromArtifact("MoneyVaultFactory");
 const TokenFactory = contract.fromArtifact("TokenFactory");
+const MoneyVault = contract.fromArtifact("MoneyVault");
+const InsureeCoin = contract.fromArtifact("InsureeCoin");
+const InvestorCoin = contract.fromArtifact("InvestorCoin");
 
 describe("InsuranceFactory", function() {
   const [controller, investor1, insuree1] = accounts;
 
   const amount = ether("1");
+
+  const signatureEnd = ether("999999999").toString();
 
   context("once deployed", function() {
     beforeEach(async function() {
@@ -43,6 +48,68 @@ describe("InsuranceFactory", function() {
         );
         expectEvent(deployReceipt, "InsuranceCreated");
         expectEvent(deployReceipt, "InsuranceCreatedDetails");
+      });
+
+      it("investor invests in moneyFactory", async function() {
+        const deployReceipt = await this.insuranceFactory.createInsuranceFor(
+          "Cov Investor 05/2020 10%",
+          "Cov Insuree 05/2020 10%",
+          "0",
+          "1",
+          "2",
+          signatureEnd,
+          "10"
+        );
+
+        const moneyVault = deployReceipt.logs[0].args.moneyVault;
+        const investorCoin = deployReceipt.logs[0].args.investorCoin;
+        const investorCoinContract = await InvestorCoin.at(investorCoin);
+
+        const moneyVaultContract = await MoneyVault.at(moneyVault);
+        await moneyVaultContract.investorDeposits({
+          from: insuree1,
+          value: amount
+        });
+
+        const investorCoinTotalSupply = await investorCoinContract.totalSupply();
+
+        expect(investorCoinTotalSupply.toString()).to.equal(
+          ether("1000").toString()
+        );
+      });
+
+      it("insuree invest in moneyFactory", async function() {
+        const deployReceipt = await this.insuranceFactory.createInsuranceFor(
+          "Cov Investor 05/2020 10%",
+          "Cov Insuree 05/2020 10%",
+          "0",
+          "1",
+          "2",
+          signatureEnd,
+          "10"
+        );
+
+        const moneyVault = deployReceipt.logs[0].args.moneyVault;
+        const insureeCoin = deployReceipt.logs[0].args.insureeCoin;
+        const insureeCoinContract = await InsureeCoin.at(insureeCoin);
+
+        const moneyVaultContract = await MoneyVault.at(moneyVault);
+
+        await moneyVaultContract.investorDeposits({
+          from: insuree1,
+          value: amount
+        });
+
+        await moneyVaultContract.insureeDeposits({
+          from: insuree1,
+          value: amount
+        });
+
+        const insureeCoinTotalSupply = await insureeCoinContract.totalSupply();
+
+        expect(insureeCoinTotalSupply.toString()).to.equal(
+          ether("100").toString()
+        ); //TODO: mul1000 is just for testnet
       });
     });
   });
