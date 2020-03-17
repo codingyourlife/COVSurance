7e6bb04a180197351e09a838f7dfeb49d1ac4fed
waghD <33834105+waghD@users.noreply.github.com>
2020-03-17 13:51:12 +0100

    reduce app size

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router'
 import { AdminLayoutComponent } from './shared/components/layouts/admin-layout/admin-layout.component'
+import { AuthLayoutComponent } from './shared/components/layouts/auth-layout/auth-layout.component'
 
 export const rootRouterConfig: Routes = [
   {
@@ -56,7 +57,21 @@ export const rootRouterConfig: Routes = [
     ],
   },
   {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '404',
+        loadChildren: () =>
+          import('./views/sessions/sessions.module').then(
+            m => m.SessionsModule,
+          ),
+        data: { title: 'Invest', breadcrumb: 'INVEST' },
+      },
+    ],
+  },
+  {
     path: '**',
-    redirectTo: 'sessions/404',
+    redirectTo: '/404',
   },
 ]
